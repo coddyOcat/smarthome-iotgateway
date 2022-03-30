b692e645d1d77e3ce04cc37ed306a5c8a8148158
Đặng Hoài Bão <dang.hbaokody@gmail.com>
2022-03-30 15:11:02 +0700

    fix microbit

--- src/microbit/main.py
+++ src/microbit/main.py
@@ -89,6 +89,26 @@ def message(feed_id, feed_value):
     if isMicrobitConnected:
         ser.write(("!" + feed_id + ":" + str(feed_value) + "#").encode())
 
+
+from flask import Flask, render_template, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/post", methods = ['POST'])
+def post():
+    # if request.method == "GET":
+    #     return jsonify(recipes)
+    if request.method == "POST":
+        content = request.get_json()
+        print("Nhận: ", content)
+        message(content.feed_id, content.feed_value)
+        return jsonify("OK")
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=8989, debug=True)
+
 # room-light-1 "0" "1"
 # fan-mode "0" "1"
 # fan-1 "0" "1"
--- src/server/service/microbit.js
+++ src/server/service/microbit.js
@@ -1,15 +1,32 @@
 const path = require("path")
 let { PythonShell } = require('python-shell')
 
-module.exports =  (feed_id, feed_value) => {
-    var options = {
-        pythonOptions: ['-u'], // get print results in real-time
-            scriptPath: __dirname,
-        args: [feed_id, feed_value]
-    }
-
-    PythonShell.run("microbit.py", options, function(err, data) {
-        if (err) console.log(err)
-        else console.log(data)
+const axios = require("axios")
+const sendData = async (feed_id, feed_value) => {
+    axios.post('http://127.0.0.1:8989/post', {
+        feed_id: feed_id,
+        feed_value: feed_value
+    })
+    .then(function (response) {
+      const data = response.data;
+      console.log(data)
+    })
+    .catch(function (error) {
+      console.log(error);
     })
 }
+
+module.exports =  (feed_id, feed_value) => {
+    // var options = {
+    //     pythonOptions: ['-u'], // get print results in real-time
+    //         scriptPath: __dirname,
+    //     args: [feed_id, feed_value]
+    // }
+
+    // PythonShell.run("microbit.py", options, function(err, data) {
+    //     if (err) console.log(err)
+    //     else console.log(data)
+    // })
+    sendData(feed_id, feed_value)
+}
+
