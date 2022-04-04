5d3745bede7fc7f607fc0971533b3a11348fa916
Đặng Hoài Bão <dang.hbaokody@gmail.com>
2022-04-05 01:13:32 +0700

    fix main.py

--- src/microbit/main.py
+++ src/microbit/main.py
@@ -1,156 +1,90 @@
 import sys, os
+# import time
+import threading
 
-sys.path.append(os.path.join(os.getcwd(),"src","microbit","service"))
-import request
-
-sys.stdout.reconfigure(encoding='utf-8')    #for utf-8 encoding
-import time
-import random
 import serial.tools.list_ports
 
-# AIO_FEED_ID = ["fan-1", "fan-mode", "room-light-1", "security-mode"]
-# AIO_USERNAME = "minhtu1302"
-# AIO_KEY = "aio_ZwIw947m4Oy203LXaGOkbGxAysJt"
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
+sys.path.append(os.path.join(os.getcwd(),"src","microbit","service"))
+import requestapi
 
-# Global Variable
-# AUTO_MODE = 1
-# MANUAL_MODE = 0
-# fan_mode = AUTO_MODE
+sys.stdout.reconfigure(encoding='utf-8')
 
-# client = MQTTClient(AIO_USERNAME, AIO_KEY)
-# client.on_connect = connected
-# client.on_disconnect = disconnected
-# client.on_message = message
-# client.on_subscribe = subscribe
-# client.connect()
-# client.loop_background()
-# time.sleep(2)
-
-isMicrobitConnected = False
-
-# config port COM1, COM2, ... (dont care)
-if getPort() != " None ":
-    ser = serial.Serial( port = getPort() , baudrate = 115200)
-    isMicrobitConnected = True
-
-mess = ""
-
-while True:
-    if isMicrobitConnected:
-        readSerial()
-        # time.sleep(1)
-
-# config port COM1, COM2, ... (dont care)
 def getPort():
     ports = serial.tools.list_ports.comports()
-    N = len( ports )
-    commPort = " None "
+    N = len(ports)
+    commPort = None
     for i in range(0, N):
         port = ports [i]
-        strPort = str( port )
-        if " USB Serial Device " in strPort:
+        strPort = str(port)
+        if "USB Serial Device" in strPort:
             splitPort = strPort.split(" ")
-            commPort =( splitPort [0])
+            commPort =(splitPort[0])
     return commPort
 
-# Read data from microbit
+def connect():
+    if getPort():
+        ser = serial.Serial(port = getPort(), baudrate = 115200)
+        return ser
+    return None
+
+def processData(data):
+    data = data.replace("!", "")
+    data = data.replace("#", "")
+    splitData = data.split(":")
+    if len(splitData) > 1:
+        # splitData[0]: field name
+        # splitData[1]: value
+        requestapi.updateData(splitData[0], splitData[1])
+
+mess = ""
 def readSerial():
-    bytesToRead = ser.inWaiting()
+    bytesToRead = device.inWaiting()
     if( bytesToRead > 0):
         global mess
-        mess = mess + ser.read(bytesToRead).decode("UTF-8")
+        mess = mess + device.read(bytesToRead).decode("UTF-8")
         while ("#" in mess) and ("!" in mess):
             start = mess.find("!")
             end = mess.find("#")
-            processData( mess[start : end+1] )
+            processData(mess[start : end+1])
             if end == len(mess):
                 mess = ""
             else:
                 mess = mess[end+1 :]
 
-
-# Process data from microbit
-def processData(data):
-    print(data)
-    data = data.replace("!", "")
-    data = data.replace("#", "")
-    splitData = data.split(":")
-    # print(splitData)
-    if len(splitData) > 1:
-        # splitData[0]: field name
-        # splitData[1]: value
-
-        # to publish message to adafruit
-        # client.publish(splitData[0], splitData[1])
-        request.updateData(splitData[0], splitData[1])
-
 def message(feed_id, feed_value):
-    if isMicrobitConnected:
-        ser.write(("!" + feed_id + ":" + str(feed_value) + "#").encode())
+    device.write(("!" + feed_id + ":" + str(feed_value) + "#").encode())
 
-
-from flask import Flask, render_template, jsonify, request
-from flask_cors import CORS
-
-app = Flask(__name__)
-CORS(app)
-
-@app.route("/post", methods = ['POST'])
-def post():
-    # if request.method == "GET":
-    #     return jsonify(recipes)
-    if request.method == "POST":
-        content = request.get_json()
-        print("Nhận: ", content)
-        message(content.feed_id, content.feed_value)
-        return jsonify("OK")
-
-if __name__ == '__main__':
+def runApi():
     app.run(host='localhost', port=8989, debug=True)
 
-# room-light-1 "0" "1"
-# fan-mode "0" "1"
-# fan-1 "0" "1"
-# security-mode
-# light-mode
+if __name__ == "__main__":
 
-# def connected(client):
-#     for feed in AIO_FEED_ID:
-#         client.subscribe(feed)
-#     print("Kết nối thành công r nha =)))")
+    if connect():
+        global app
+        app = Flask(__name__)
+        CORS(app)
 
-# def subscribe(client, userdata, mid, granted_qos):
-#     print("subcribe thành công.. .")
+        global device
+        device = connect()
 
-# def disconnected(client):
-#     print("Ngắt kết nối.. .")
-#     sys.exit(1)
+        @app.route("/post", methods = ['POST'])
+        def post():
+            # if request.method == "GET":
+            #     return jsonify(recipes)
+            if request.method == "POST":
+                content = request.get_json()
+                print("Nhận: ", content)
+                message(content.feed_id, content.feed_value)
+                return jsonify("OK")
 
-# process data from adafruit to gateway
-# def message(client, feed_id, payload):
-#     # payload is value that adafruit/web/app send to gateway
+        httpReponse = threading.Thread(target = runApi)
+        httpReponse.start()
 
-#     # fan mode: auto or manual
-#     ## auto: if hot, fan on, else fan off (do inside microbit, human cannot set the fan speed)
-#     ##### In auto mode, microbit will publish the current fanspeed to adafruit
-#     ##### and gateway won't hear anything from fanspeed topic from adafruit/web/app (prevent forever loop)
+        while True:
+            readSerial()
 
-#     ## manual: human can set fan speed through web or app
-#     ##### In manual mode, microbit will recieve fanspeed from adafruit/wweb/app
-#     global fan_mode
-#     print("Nhận dữ liệu từ " + feed_id + ": " + payload)
-#     if isMicrobitConnected:
-#         if fan_mode == AUTO_MODE and feed_id == "fan-1":
-#             # In auto mode, gateway won't hear anything from fanspeed topic from adafruit/web/app
-#             pass
-#         else:
-#             if feed_id == "fan-mode":
-#                 # Update fan mode
-#                 fan_mode = payload
-#             ser.write(("!" + feed_id + ":" + str(payload) + "#").encode())
-
-
-
-
-
+    else:
+        sys.exit()
